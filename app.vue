6214e923f83d2f998003ce96f19ca2a22c78fdb4
Pihkaal <pihkaal@proton.me>
2024-10-17 22:02:07 +0200

    feat: seo meta and favicon

--- app.vue
+++ app.vue
@@ -1,3 +1,11 @@
+<script setup lang="ts">
+useSeoMeta({
+  title: "Lilou Chat",
+  description: "Just ✨ Lilou ✨",
+  themeColor: "fa86c4",
+});
+</script>
+
 <template>
   <div class="h-screen">
     <AppBody />
